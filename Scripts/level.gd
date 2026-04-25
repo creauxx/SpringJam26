ae4bd9127d69621df6c16b48020b282afbb9f41d
creauxx <creaux3@gmail.com>
2026-04-25 11:33:19 -0600

    4.25.26_1

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -2,8 +2,15 @@ extends Node2D
 
 @onready var mc = $MC
 @onready var bee = $Bee
+@onready var sunflower = $Sunflower
 @onready var game_ui = $PlayerUI
 @onready var tile_map = $BaseLayer
+@onready var flower_1 = $Flower1
+@onready var flower_2 = $Flower2
+@onready var flower_3 = $Flower3
+@onready var flower_4 = $Flower4
+@onready var flower_5 = $Flower5
+@onready var flower_6 = $Flower6
 
 @export var initial_delay: float = 0.15
 
@@ -12,35 +19,73 @@ var current_dir: Vector2i = Vector2i.ZERO
 var tile_size: int = 16
 var repeat_timer: float = 0.0
 var halt: bool = false
+var offset_pos: Vector2 = Vector2(8, 8)
 #story toggles
 var bool_bee_1: bool = false
 var bool_bee_2: bool = false
+var bool_sunflower_1: bool = false
+var bool_first_flower: bool = true
+var bool_flower_1: bool = false
+var bool_flower_2: bool = false
+var bool_flower_3: bool = false
+var bool_flower_4: bool = false
+var bool_flower_5: bool = false
+var bool_flower_6: bool = false
 #sprite vars
 var mc_sprite: Texture2D = preload("res://Art/png/mc sprite.png")
 var mc_sprite_1: Texture2D = preload("res://Art/png/mc sprite 1.png")
 var bee_sprite: Texture2D = preload("res://Art/png/bee.png")
 #dialogue
 var dialogue_bee = preload("res://Dialogue/bee.dialogue")
+var dialogue_flowers = preload("res://Dialogue/flowers.dialogue")
+var dialogue_sunflowers = preload("res://Dialogue/sunflowers.dialogue")
 
 
 func _ready():
 	pass
 	
 func _process(delta: float):
-	if mc.position == (Vector2(96, 80) + Vector2(8, 8)) and not halt and not bool_bee_1:
+##Batrice the Bee
+	if mc.position == (Vector2(96, 80) + offset_pos) and not halt and not bool_bee_1:
 		halt = true
 		bool_bee_1 = true
 		DialogueManager.show_example_dialogue_balloon(dialogue_bee, "start", [self])
 		await DialogueManager.dialogue_ended		
 		current_dir = Vector2i.ZERO
 		halt = false
-	if mc.position == (Vector2(16, 80) + Vector2(8, 8)) and not halt and bool_bee_1 and not bool_bee_2:
+	if mc.position == (Vector2(16, 80) + offset_pos) and not halt and bool_bee_1 and not bool_bee_2:
 		halt = true
 		bool_bee_2 = true
 		DialogueManager.show_example_dialogue_balloon(dialogue_bee, "bee_1_b", [self])
 		await DialogueManager.dialogue_ended		
 		current_dir = Vector2i.ZERO
 		halt = false
+##Sunny the Sunflower
+	if (mc.position == (sunflower.position) + Vector2(-16, 0) or mc.position == (sunflower.position) + Vector2(0, 16) or mc.position == (sunflower.position) + Vector2(0, -16)) and not halt and not bool_sunflower_1:
+		halt = true
+		bool_sunflower_1 = true
+		DialogueManager.show_example_dialogue_balloon(dialogue_sunflowers, "start", [self])
+		await DialogueManager.dialogue_ended	
+		sunflower.position = Vector2(48, 144) + offset_pos
+		current_dir = Vector2i.ZERO
+		halt = false
+##Flowers
+	if (mc.position == flower_1.position + Vector2(16, 0) or mc.position == flower_1.position + Vector2(0, 16) or mc.position == flower_1.position + Vector2(-16, 0) or mc.position == flower_1.position + Vector2(0, -16)) and not bool_flower_1:
+		halt = true
+		bool_flower_1 = true
+		if bool_first_flower:
+			DialogueManager.show_example_dialogue_balloon(dialogue_flowers, "first", [self])
+			await DialogueManager.dialogue_ended
+			bool_first_flower = false				
+		else:
+			DialogueManager.show_example_dialogue_balloon(dialogue_flowers, "flower1", [self])
+			await DialogueManager.dialogue_ended	
+		flower_1.position = Vector2(bee.position) + Vector2(0, -16) + Vector2(-4, 0)
+		#flower_1.rotation = -90
+		game_ui.unhide_flowers(0)
+		current_dir = Vector2i.ZERO
+		halt = false
+##MC movement
 	if halt: return
 	if current_dir != Vector2i.ZERO:
 		repeat_timer -= delta
@@ -51,7 +96,7 @@ func _process(delta: float):
 
 func _unhandled_input(event: InputEvent):
 	if halt: return
-	if event.is_action_pressed("Q"): mc.position = Vector2(112, 80) + Vector2(8, 8)
+	if event.is_action_pressed("Q"): mc.position = Vector2(112, 80) + offset_pos
 	var input_dir = Vector2i.ZERO
 	if event.is_action_pressed("ui_right"): input_dir.x = 1 
 	elif event.is_action_pressed("ui_left"): input_dir.x = -1
@@ -68,7 +113,7 @@ func _unhandled_input(event: InputEvent):
 
 
 func move_mc(dir):
-	var cell: Vector2 = Vector2(mc.position) + Vector2(dir * tile_size) - Vector2(8, 8)
+	var cell: Vector2 = Vector2(mc.position) + Vector2(dir * tile_size) - offset_pos
 	var data = tile_map.get_cell_tile_data(Vector2i(Vector2(cell) / tile_size))
 	if data:
 		var is_solid = data.get_custom_data("is_solid")
@@ -81,4 +126,4 @@ func reward_bee_1_a():
 	game_ui.update_texture(mc.texture)
 	
 func move_bee_1():
-	bee.position = Vector2(0, 80) + Vector2(8, 8)
+	bee.position = Vector2(0, 80) + offset_pos
--- Scripts/player_ui.gd
+++ Scripts/player_ui.gd
@@ -1,6 +1,18 @@
 extends CanvasLayer
 
-@onready var tex = $Control/TextureRect
+@onready var mc_tex = $Control/TextureRect
+@onready var flower_array: Array = [
+	$Control/Flower1, 
+	$Control/Flower2,
+	$Control/Flower3,
+	$Control/Flower4,
+	$Control/Flower5,
+	$Control/Flower6
+]
 
 func update_texture(new_texture: Texture2D):
-	tex.texture = new_texture
+	mc_tex.texture = new_texture
+
+
+func unhide_flowers(array_num: int):
+	flower_array[array_num].show()
